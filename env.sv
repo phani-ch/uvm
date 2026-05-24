0fa743032a1220b9edb0282400f678c58de10ec6
phani-ch <86233846+phani-ch@users.noreply.github.com>
2026-05-24 10:18:25 +0530

    Refactor my_env class structure
    Removed agent and scoreboard instantiation from my_env class.

--- env.sv
+++ env.sv
@@ -1,21 +1,11 @@
 class my_env extends uvm_env;
-  `uvm_component_utils(my_env);
-  
-  my_agent ma;
-//   my_scoreboard ms;
-  
-  function new(string name="my_env", uvm_component parent=null);
-    super.new(name,parent);
-  endfunction
-  
-  function void build_phase(uvm_phase phase);
-    super.build_phase(phase);
+    `uvm_component_utils(my_env)
     
-    ma=my_agent :: type_id :: create("ma",this);
-    // ms=my_scoreboard :: type_id :: create("ms",this);
+  
+    
+    function new(string name, uvm_component parent);
+        super.new(name, parent);
+    endfunction
    
-  endfunction 
-  
- 
-  
+
 endclass
