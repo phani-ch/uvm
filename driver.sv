651783e8c017762610d92e21b428ea04f7a19b3a
phani-ch <86233846+phani-ch@users.noreply.github.com>
2026-05-24 10:17:52 +0530

    Refactor my_driver class and remove build_phase
    Removed unused build_phase function and updated item type.

--- driver.sv
+++ driver.sv
@@ -1,13 +1,10 @@
-class my_driver extends uvm_driver #(my_sequ_item); // Aapka seq_item use ho raha hai
-  `uvm_component_utils(my_driver)
+class my_driver extends uvm_driver#(my_item);
 
-    function new(string name="my_driver", uvm_component parent=null);
-    super.new(name,parent);
-  endfunction
+    `uvm_component_utils(my_driver)
 
-   virtual function void build_phase(uvm_phase phase);
-    super.build_phase(phase);
-  
-  endfunction
+    function new(string name ="my_driver",uvm_component parent);
+        super.new(name,parent);
+    endfunction 
 
-endclass 
+
+endclass
