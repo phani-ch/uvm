fc903c0e195d37cea3a42de98d06e3c753a8fee8
phani-ch <86233846+phani-ch@users.noreply.github.com>
2026-05-24 10:17:25 +0530

    Refactor my_agent class to remove unnecessary code
    Removed implementation details from my_agent class.

--- agent.sv
+++ agent.sv
@@ -1,26 +1,10 @@
 class my_agent extends uvm_agent;
-  `uvm_component_utils(my_agent);
-  my_sequencer ms;
-  my_driver md;
-  my_monitor mm;
-  
-  function new(string name="my_agent", uvm_component parent=null);
-    super.new(name,parent);
-  endfunction
-  
-  function void build_phase (uvm_phase phase);
-    super.build_phase(phase);
-    mm=my_monitor :: type_id :: create("mm",this);
-    
-    if(get_is_active() == UVM_ACTIVE) begin
-    md=my_driver :: type_id :: create("md",this);
-    ms=my_sequencer :: type_id :: create("ms",this);
-    end
-  endfunction
-  
-  
-  function void connect_phase (uvm_phase phase);
-    super.connect_phase(phase);
-    
-  endfunction
+
+    `uvm_component_utils(my_agent)
+
+
+
 endclass
+
+
+
