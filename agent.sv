08194bbd8013dd5a84b53a8ae183f79d8c288c59
phani-ch <86233846+phani-ch@users.noreply.github.com>
2026-05-25 11:56:27 +0530

    Implement connect_phase in my_agent class
    Added connect_phase function to my_agent class for connecting driver and sequencer.

--- agent.sv
+++ agent.sv
@@ -12,11 +12,16 @@ class my_agent extends uvm_agent;
   my_monitor mtr;
 
     function void build_phase(uvm_phase phase);
-        super.build_phase(phase);
+             super.build_phase(phase);
              seqr = my_sequencer::type_id::create("sequencer", this);
              drv = my_driver::type_id::create("driver", this);
              mtr= my_monitor::type_id::create("monitor", this);
-  endfunction
+     endfunction
+
+      function void connect_phase(uvm_phase phase);
+             super.connect_phase(phase);
+             driver.seq_item_port.connect(sequencer.seq_item_export);
+       endfunction
 endclass
 
 
