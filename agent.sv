4477e137225c23d6143dbb736b389f52dc267dcc
phani-ch <86233846+phani-ch@users.noreply.github.com>
2026-05-25 11:50:19 +0530

    Update agent.sv

--- agent.sv
+++ agent.sv
@@ -3,7 +3,9 @@ class my_agent extends uvm_agent;
     `uvm_component_utils(my_agent)
 
 
-
+  function new(string name, uvm_component parent);
+    super.new(name, parent);
+  endfunction
 endclass
 
 
