abfd802645b05808fbae6d728630ad8f48d80388
phani-ch <86233846+phani-ch@users.noreply.github.com>
2026-05-25 12:07:49 +0530

    Enhance my_test class with build and elaboration phases

--- test.sv
+++ test.sv
@@ -1,11 +1,21 @@
 class my_test extends uvm_test;
+  `uvm_component_utils(my_test)
 
-    `uvm_component_utils(my_test)
-  
+  my_env env;
 
+  function new(string name, uvm_component parent);
+    super.new(name, parent);
+  endfunction
 
-    function new(string name = "my_test",uvm_component parent);
-        super.new(name,parent);
-    endfunction 
+  function void build_phase(uvm_phase phase);
+    super.build_phase(phase);
+    env = my_env::type_id::create("env", this);
+  endfunction
 
+    function void end_of_elaboration_phase(uvm_phase phase);
+    super.end_of_elaboration_phase(phase);
+    uvm_top.print_topology();
+  endfunction
+
+    
 endclass 
