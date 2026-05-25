75e282a58025f0c274bfebe83f149921bc81b11e
phani-ch <86233846+phani-ch@users.noreply.github.com>
2026-05-25 12:02:49 +0530

    Implement build_phase in my_env class
    Added build_phase function to my_env class to create an agent.

--- env.sv
+++ env.sv
@@ -6,6 +6,11 @@ class my_env extends uvm_env;
     function new(string name, uvm_component parent);
         super.new(name, parent);
     endfunction
+
+    function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+        agent = my_agent::type_id::create("agent", this);
+    endfunction
    
 
 endclass
